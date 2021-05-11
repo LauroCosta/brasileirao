424bf01d89d5ad22462264303403d18e164acc60
Lauro Costa <joaolauro94@gmail.com>
2021-05-11 00:49:30 -0300

    created component macthCard

--- src/components/MatchCard/index.tsx
+++ src/components/MatchCard/index.tsx
@@ -1,5 +1,30 @@
+import { Container } from "./style";
+
 export function MatchCard() {
   return (
-    <h1>teste</h1>
-  )
+    <Container>
+      <div className="dateMacth">
+        <span>11/02/2021</span>
+      </div>
+
+      <div className="macth">
+        <span>ACG</span>
+        <div className="circle">
+          <img
+            src="https:/apifutebol.s3.sa-east-1.amazonaws.com/escudos/5f999db078911.svg"
+            alt=""
+          />
+        </div>
+        <span>X</span>
+        <div className="circle">
+          <img
+            src="https:/apifutebol.s3.sa-east-1.amazonaws.com/escudos/5f999d7ab48fa.svg"
+            alt=""
+          />
+        </div>
+        <span>COR</span>
+      </div>
+
+    </Container>
+  );
 }
--- src/components/MatchCard/style.ts
+++ src/components/MatchCard/style.ts
@@ -1,3 +1,59 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 22rem;
+  height: 10rem;
+  background: #ffff;
+  border-radius: 0.5rem;
+  border: 1px solid #ededed;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  transition: transform 0.2s;
+
+  &:hover {
+    transform: scale(1.02);
+  }
+
+
+  .dateMacth {
+    margin-bottom: 1rem;
+    
+    span {
+      color: #404040;
+      font-size: 1rem;
+    }
+  }
+
+  .macth {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 2rem;
+  
+    span {
+      font-weight: bolder;
+      color: #404040;
+      font-size: 1.25rem;
+      padding: 0.5rem;
+    }
+
+    .circle {
+      background: #ffffff;
+      height: 4rem;
+      width: 4rem;
+      border-radius: 100%;
+      border: 1px solid #e5e5e5;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      img {
+        width: 2.5rem;
+      }
+    }
+  }
+`;
