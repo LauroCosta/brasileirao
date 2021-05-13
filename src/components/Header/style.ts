2c6ccb98a53bf27d2be17f3b63ca909dc8b43ccb
Lauro Costa <joaolauro94@gmail.com>
2021-05-13 00:35:15 -0300

    created context classification

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
+  
   background: #ffff;
   height: 5rem;
   border-bottom: 1px solid #ededed;
@@ -30,9 +31,34 @@ export const Container = styled.header`
         font-size: 1.5rem;
         color: #0049A9;
       }
-
-
     }
 
   }
+
+  .teams {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    top: 5rem;
+    position: relative;
+    background: #cbcbcb;
+    padding: 0.5rem 1rem;
+    border-radius: 0 0 2rem 2rem;
+
+    a {
+      padding: 0.25rem;
+      img {
+        width: 3rem;
+        height: 3rem;
+        filter: grayscale(1);
+        filter: brightness(0.8);
+        transition: filter 0.2s;
+
+        &:hover {
+          filter: brightness(1);
+          padding-bottom: 0.25rem;
+        }
+      }
+    }
+  } 
 `;
